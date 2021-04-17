b4068c302dbab8edab2f675a15ea41d2434207a0
henrikolsvik <henrik-olsvik@outlook.com>
2021-04-17 14:08:21 +0200

    removed import updated github config for lstm

--- scripts/ml/lstm.py
+++ scripts/ml/lstm.py
@@ -1,11 +1,9 @@
 import sys
 
-from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.python.keras.layers import Dropout
 from tensorflow.python.keras.models import Sequential
 import numpy as np
-from tensorflow.keras import regularizers
 
 from ml_interface import Mlinterface
 
@@ -21,24 +19,15 @@ class LSTM(Mlinterface):
         train_sample, train_target, test_sample, test_target, test_name = \
             self.n_split_shuffle(read_samples_with_names, target, int(self.config["n"]))
 
-        max_value = 0
-        for set in train_sample:
-            for item in set:
-                if max(item) > max_value:
-                    max_value = int(max(item))+1
-        max_value = max_value + int(self.config["lstm_normalization_multiplier"])
-
         score = []
 
         for i in range(0, len(train_sample)):
             iter_train_sample = [train_sample[i]]
             iter_train_sample = np.array(iter_train_sample)
             iter_train_sample = iter_train_sample.reshape(len(iter_train_sample[0]), 1, len(iter_train_sample[0][0])).tolist()
-            #iter_train_sample = np.reshape(iter_train_sample, (iter_train_sample.shape[0], 1, iter_train_sample.shape[1]))
             iter_train_target = [int(k) for k in train_target[i]]
 
             model = Sequential()
-            #model.add(layers.Embedding(max_value, int(self.config["embedding"]), input_shape=(len(train_sample[0][0]), )))#, input_shape=(len(train_sample[0][0]), ))) #Max value, 32,
             for q in range(1, int(self.config["num_layers"])):
                 model.add(layers.LSTM(int(self.config["LSTM_depth"]), return_sequences=True))
                 model.add(Dropout(0.2))
@@ -54,7 +43,6 @@ class LSTM(Mlinterface):
             iter_test_sample = iter_test_sample.reshape(len(iter_test_sample[0]), 1, len(iter_test_sample[0][0])).tolist()
             iter_test_target = [int(k) for k in test_target[i]]
 
-            #model.fit(iter_train_sample, iter_train_target, batch_size=int(self.config["batch_size"]), epochs=int(self.config["epochs"]))
             score.append(model.evaluate(iter_test_sample, iter_test_target, verbose=2)[1])
 
         self.write_results(output_filename, input_samples_file, input_samples_parameters_file, score, target)
